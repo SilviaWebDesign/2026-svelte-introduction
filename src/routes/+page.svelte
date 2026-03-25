c2e9dbf7a32a9ec4a76fb1c434e01bc0d5508556
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2026-03-25 17:12:34 +0100

    add style local and global

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
+
+<script>
+    
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style>
+    h1 {
+        color: rgb(21, 42, 195);
+    }
+</style>
