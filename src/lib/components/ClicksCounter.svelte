39be50d4d0fd637a9ccd37e5678565d2111d6ccc
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2026-03-25 17:40:13 +0100

    create ckickscunter component

---
+++ src/lib/components/ClicksCounter.svelte
@@ -0,0 +1,16 @@
+<script>
+
+    let count = $state(0)
+    let doubled = $derived(count* 2)
+
+</script>
+
+
+<h3>Clicks counter</h3>
+
+
+<button on:click={() => count++}>
+    clicks: {count}
+</button>
+
+<span> doubled is {doubled} </span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 
 <script>
-    
+
+    import ClicksCounter from "$lib/components/ClicksCounter.svelte";
+    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
 </script>
 
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
+<ClicksCounter />
 
 <style>
     h1 {
