59a5cbd2b326d70c8c18edb00be5c33da08fd7c7
SilviaWebDesign <silvia.lamastra@mail.polimi.it>
2026-04-08 17:32:21 +0200

    fix canvas with onMouth

---
+++ src/lib/components/CanvasSquare.svelte
@@ -0,0 +1,56 @@
+<script>
+    import {untrack} from "svelte"
+
+    let size= $state(50)
+    let color= $state("#ff3e00")
+
+    let canvas 
+
+    $effect(() => {
+        const context = canvas.getContext("2d")
+        context.clearRect(0,0, canvas.width, canvas.height)
+
+        context.fillStyle = untrack(() => color)
+        context.fillRect(0,0, size, size)
+    })
+
+    // let canvas = document.querySelector("canvas")
+    
+    // on mounth - montato pronto
+    // effect - reazione
+
+</script>
+
+<h3>Canvas Square</h3>
+
+<article>
+    <canvas bind:this={canvas} width="100" height="100"></canvas>
+
+    <nav>
+        <label>
+            Size: <input type="range" bind:value={size}>
+        </label>
+
+         <label>
+            Color: <input type="color" bind:value={color}>
+        </label>
+    </nav>
+</article>
+
+<style>
+    article {
+        display: flex;
+        gap: len;
+        align-items: flex-end;
+    }
+
+    canvas {
+        border: solid 1px black;
+    }
+
+    nav {
+        display: flex;
+        gap: len;
+        flex-direction: column;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
 
 <script>
-
+    import CanvasSquare from "$lib/components/CanvasSquare.svelte";
     import ClicksCounter from "$lib/components/ClicksCounter.svelte";
-    import ProgressiveSum from "$lib/components/ProgressiveSum.svelte";
-
+    
     let count = $state(5)
 </script>
 
@@ -11,10 +10,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<ClicksCounter initialCount={count} />
+<ClicksCounter bind:initialCount={count} />
 <!-- <p> count from parent: {count}</p> -->
 
 <p>count from parent is {count}</p>
+
+<CanvasSquare/>
 
 <style>
     h1 {
